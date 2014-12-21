071cb049445647afff6251bc6291c8499303dc64
sherlockhua <sherlockhua@163.com>
2014-12-21 17:27:01 +0800

    add it

--- config.cmake
+++ config.cmake
@@ -9,6 +9,7 @@ file(GLOB CXX_SOURCES "./src/*.cpp" "./src/response/*.cpp")
 #file(GLOB PHP_SOURCES "./src/*.php")
 
 
+
 HHVM_EXTENSION("yaf" ${CXX_SOURCES})
 HHVM_SYSTEMLIB("yaf" "./src/ext_yaf.php")
 #HHVM_SYSTEMLIB("yaf" ${PHP_SOURCES})
