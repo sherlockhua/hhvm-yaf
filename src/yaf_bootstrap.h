331376da4a6b6ff582eb3e43f6ea3a168b1fed0d
张华 <sherlockhua@163.com>
2015-01-30 00:15:46 +0800

    add it

---
+++ src/yaf_bootstrap.h
@@ -0,0 +1,22 @@
+/*===========================================
+*   Copyright (C) 2015 All rights reserved.
+*   
+*   company      : xiaomi
+*   author       : sherlockhua
+*   email        : sherlockhua@xiaomi.com
+*   date         ：2015-01-29 20:38:46
+*   description  ：
+*
+=============================================*/
+#ifndef _YAF_BOOTSTRAP_H_
+#define _YAF_BOOTSTRAP_H_
+
+
+namespace HPHP{
+
+#define YAF_DEFAULT_BOOTSTRAP           "Bootstrap"
+#define YAF_DEFAULT_BOOTSTRAP_LOWER     "bootstrap"
+#define YAF_DEFAULT_BOOTSTRAP_LEN       10
+#define YAF_BOOTSTRAP_INITFUNC_PREFIX   "_init"
+
+}
