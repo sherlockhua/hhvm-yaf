43c44a3a7e3abefeea5ba7744294ed97429538c1
张华 <sherlockhua@163.com>
2015-01-30 16:47:43 +0800

    add it

---
+++ src/yaf_router.h
@@ -0,0 +1,27 @@
+/*===========================================
+*   Copyright (C) 2015 All rights reserved.
+*   
+*   company      : xiaomi
+*   author       : sherlockhua
+*   email        : sherlockhua@xiaomi.com
+*   date         ：2015-01-30 11:13:14
+*   description  ：
+*
+=============================================*/
+#ifndef _YAF_ROUTER_H_
+#define _YAF_ROUTER_H_
+
+#define YAF_ROUTER_DEFAULT_ACTION       "index"
+#define YAF_ROUTER_DEFAULT_CONTROLLER   "Index"
+#define YAF_ROUTER_DEFAULT_MODULE       "Index"
+#define YAF_DEFAULT_EXT                 "php"
+
+#define YAF_ROUTER_PROPERTY_NAME_ROUTERS        "_routes"
+#define YAF_ROUTER_PROPERTY_NAME_CURRENT_ROUTE  "_current"
+
+
+
+
+
+
+#endif //_YAF_ROUTER_H
