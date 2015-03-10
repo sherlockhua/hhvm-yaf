5e3cf19e2e021ed54713e5149d822d47fe53aeba
张华 <sherlockhua@163.com>
2015-03-10 23:26:26 +0800

    fix it

---
+++ src/yaf_action.h
@@ -0,0 +1,21 @@
+/*===========================================
+*   Copyright (C) 2015 All rights reserved.
+*   
+*   company      : xiaomi
+*   author       : sherlockhua
+*   email        : sherlockhua@xiaomi.com
+*   date         ：2015-03-10 20:53:42
+*   description  ：
+*
+=============================================*/
+#ifndef _YAF_ACTION_H_
+#define _YAF_ACTION_H_
+
+#define YAF_ACTION_EXECUTOR_NAME        "execute"
+#define YAF_ACTION_PROPERTY_NAME_CTRL "_controller"
+
+
+
+
+
+#endif //_YAF_ACTION_H
